abf7814bd6399628e5258b4b24744071cf90b19b
DoA-ua <DoA-ua@.github.com>
2017-08-31 13:32:53 +0300

    Initial commit

---
+++ dvigatel.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<title>Двигатели</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/dvigatel.css">
+</head>
+<body>
+<div class="wrapper">
+
+			<header>
+				<nav class="s-menu">
+					<ul class="s-ul">
+					<div class="contact">
++380676277133 Олег Иванович
+</div>
+						<li><a href="index.html"><i class="fa fa-"></i>Главная</a></li>
+						<li><a href="#"><i class="fa fa-"></i>Каталог</a>
+							<ul>
+								<li><a href="dvigatel.php">Двигатели</a></li>
+								<!--<li><a href="#">Запчасти</a></li>-->
+								<!--<li><a href="#">Шины для спецтехники</a></li>-->
+								<!--<li><a href="#">Техника</a></li>-->
+							</ul>
+						</li>
+						<li><a href="contact.html"><i class="fa fa-"></i>Контакты</a></li>
+						<li><a href="#"><i class="fa fa-"></i>Корзина</a></li>
+					</ul>
+				</nav>
+			</header>
+			<div class="container">
+				<div class="cart">
+                    <p class="name">Д-21</p>
+                    <img src="https://нашмонтаж.com/components/com_virtuemart/assets/images/vmgeneral/noimage.gif" alt="" width="128px" height="128px">
+                    <div class="cost">21000 грн</div>
+                    <div class="description"></div>
+                    <button class="add-to-cart">Купить</button>
+                </div>
+                <div class="cart">
+                    <p class="name">Д-144</p>
+                    <img src="https://нашмонтаж.com/components/com_virtuemart/assets/images/vmgeneral/noimage.gif" alt="" width="128px" height="128px">
+                    <div class="cost">27000грн</div>
+                    <div class="description"></div>
+                    <button class="add-to-cart">Купить</button>
+                </div>
+                <div class="cart">
+                    <p class="name">ЯМЗ-238</p>
+                    <img src="https://нашмонтаж.com/components/com_virtuemart/assets/images/vmgeneral/noimage.gif" alt="" width="128px" height="128px">
+                    <div class="cost">65000 грн</div>
+                    <div class="description"></div>
+                    <button class="add-to-cart">Купить</button>
+                </div>
+                <div class="cart">
+                    <p class="name">ЯМЗ-238</p>
+                    <img src="https://нашмонтаж.com/components/com_virtuemart/assets/images/vmgeneral/noimage.gif" alt="" width="128px" height="128px">
+                    <div class="cost">60000 грн</div>
+                    <div class="description"></div>
+                    <button class="add-to-cart">Купить</button>
+                </div>
+                <div class="cart">
+                    <p class="name">ЯМЗ-240</p>
+                    <img src="img/1.jpg" alt="" width="128px" height="128px">
+                    <div class="cost">55000 грн</div>
+                    <div class="description"></div>
+                    <button class="add-to-cart">Купить</button>
+                </div>
+                <div class="cart">
+                    <p class="name">КАМАЗ-740</p>
+                    <img src="https://нашмонтаж.com/components/com_virtuemart/assets/images/vmgeneral/noimage.gif" alt="" width="128px" height="128px">
+                    <div class="cost">37000 грн</div>
+                    <div class="description"></div>
+                    <button class="add-to-cart">Купить</button>
+                </div>
+                <div class="cart">
+                    <p class="name">МТЗ</p>
+                    <img src="https://нашмонтаж.com/components/com_virtuemart/assets/images/vmgeneral/noimage.gif" alt="" width="128px" height="128px">
+                    <div class="cost">40000 грн</div>
+                    <div class="description"></div>
+                    <button class="add-to-cart">Купить</button>
+                </div>
+                <div class="cart">
+                    <p class="name">ЮМЗ</p>
+                    <img src="https://нашмонтаж.com/components/com_virtuemart/assets/images/vmgeneral/noimage.gif" alt="" width="128px" height="128px">
+                    <div class="cost">37000 грн</div>
+                    <div class="description"></div>
+                    <button class="add-to-cart">Купить</button>
+                </div>
+			</div>
+				<div class="footer">
+				<div class="inner-footer">Все права защищены © 2017</div>
+			</div>
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/main.js"></script>
+</div>
+</body>
+</html>
